168a89fe775ce614e23aba34d0a582b3c8fafcb0
Roman Galeev <jamhed@2600hz.com>
2017-02-16 15:06:14 +0100

    better stop script

--- stop.sh
+++ stop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 NETWORK=${NETWORK:-"kazoo"}
 echo Stopping network: $NETWORK
-docker stop -t 1 $(docker ps | grep "\.$NETWORK" | cut -d' ' -f1)
+docker stop -t 1 $(docker ps --filter name=\.${NETWORK}\$ --format {{.ID}})
