328f249dba7faaeb327d6841dea56cde88f916c0
Roman Galeev <jamhed@2600hz.com>
2017-02-16 15:06:30 +0100

    hosts script
    allows to print Kazoo's internal components ips and names

---
+++ bin/hosts.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+NETWORK=${NETWORK:-"kazoo"}
+for NAME in $(docker ps --filter name=\.${NETWORK}\$ --format {{.Names}})
+do
+	IP=`docker inspect --format "{{ (index .NetworkSettings.Networks \"$NETWORK\").IPAddress }}" $NAME`
+	echo $NAME $IP
+done
+
+
