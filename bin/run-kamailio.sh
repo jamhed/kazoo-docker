0663935cff8f636f4731c27de3ddb95d968576f5
Roman Galeev <jamhed@2600hz.com>
2017-02-16 19:49:59 +0100

    use docker to route udp 5060 to kamailio

--- bin/run-kamailio.sh
+++ bin/run-kamailio.sh
@@ -16,10 +16,7 @@ docker run $FLAGS \
 	--net $NETWORK \
 	-h $NAME \
 	--name $NAME \
+	-p 5060:5060/udp \
 	--env NETWORK=$NETWORK \
 	--env RABBITMQ=$RABBITMQ \
 	2600hz/kamailio
-
-IP=$(bin/get-ip.sh $NETWORK $NAME)
-echo -n "forwarding sip port 5060 to kamailio $IP "
-iptables -t nat -A PREROUTING -p udp --dport 5060 -j DNAT --to-destination $IP
