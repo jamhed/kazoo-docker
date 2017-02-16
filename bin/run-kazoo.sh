1b609c4ae31009040113fdecf327a248f5f82e66
Roman Galeev <jamhed@2600hz.com>
2017-02-16 14:45:43 +0100

    allow to override auto-started kazoo apps

--- bin/run-kazoo.sh
+++ bin/run-kazoo.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 FLAGS=${1:-"-td"}
 NETWORK=${NETWORK:-"kazoo"}
+KAZOO_APPS=${APPS:-"sysconf,blackhole,callflow,cdr,conference,crossbar,fax,hangups,media_mgr,milliwatt,omnipresence,pivot,registrar,reorder,stepswitch,teletype,trunkstore,webhooks,ecallmgr"}
 NAME=kazoo.$NETWORK
 if [ -n "$(docker ps -aq -f name=$NAME)" ]
 then
 	docker stop -t 1 $NAME
 	docker rm -f $NAME
 fi
+echo -n "starting kazoo with apps: $KAZOO_APPS "
 docker run $FLAGS \
 	--net $NETWORK \
 	-h $NAME \
@@ -15,5 +17,5 @@ docker run $FLAGS \
 	--env COUCHDB=couchdb.$NETWORK \
 	--env RABBITMQ=rabbitmq.$NETWORK \
 	--env NODE_NAME=kazoo \
-	--env KAZOO_APPS=sysconf,blackhole,callflow,cdr,conference,crossbar,fax,hangups,media_mgr,milliwatt,omnipresence,pivot,registrar,reorder,stepswitch,teletype,trunkstore,webhooks,ecallmgr \
+	--env KAZOO_APPS=KAZOO_APPS \
 	2600hz/kazoo
