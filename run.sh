ebedc748e6e3288f48f3f5a184e3b5152429ddac
Roman Galeev <jamhed@2600hz.com>
2017-02-16 17:58:19 +0100

    fix: guessing right uri

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ then
 	EXT_IP=$(curl -s ipinfo.io/ip)
 	echo "Guessing our Kazoo API uri: http://$EXT_IP/api/v2"
 	echo "Please specify it in KAZOO_URI env variable, e.g. http://your-server.domain, without api/v2 part, see nginx config"
+	KAZOO_URI=http://$EXT_IP
 fi
 export KAZOO_URL=${KAZOO_URL:-"$KAZOO_URI/api/v2"}
 
